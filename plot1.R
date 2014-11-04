ce411955927072cd92e036947f605b9cb4207cf7
mjcw <mjcoursework@gmail.com>
2014-11-05 01:00:49 +0530

    Adding code and plot for plot1

---
+++ plot1.R
@@ -0,0 +1,8 @@
+generatePlot1 <- function(outputFileName="plot1.png"){
+  data <- read.table("data/household_power_consumption.txt", header=TRUE, sep=";", na.strings="?")
+  data$datetime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
+  dataset <- subset(data, as.Date(datetime)=="2007-02-01" | as.Date(datetime)=="2007-02-02")
+  png(outputFileName, width=480, height=480)
+  hist(dataset$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", col="red")
+  dev.off()
+}
