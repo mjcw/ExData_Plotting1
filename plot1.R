19ccc6bf4c40b0e4921dfa5d2c0abb0a5a08d4ff
mjcw <mjcoursework@gmail.com>
2014-11-07 14:51:04 +0530

    Adding comments

--- plot1.R
+++ plot1.R
@@ -1,8 +1,12 @@
 generatePlot1 <- function(outputFileName="plot1.png"){
+  # read the data
   data <- read.table("data/household_power_consumption.txt", header=TRUE, sep=";", na.strings="?")
   data$datetime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
   dataset <- subset(data, as.Date(datetime)=="2007-02-01" | as.Date(datetime)=="2007-02-02")
+  
+  # generate plot
   png(outputFileName, width=480, height=480)
   hist(dataset$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", col="red")
   dev.off()
+  
 }
--- plot2.R
+++ plot2.R
@@ -1,7 +1,10 @@
 generatePlot2 <- function(outputFileName="plot2.png"){
+  # read the data
   data <- read.table("data/household_power_consumption.txt", header=TRUE, sep=";", na.strings="?")
   data$datetime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
   dataset <- subset(data, as.Date(datetime)=="2007-02-01" | as.Date(datetime)=="2007-02-02")
+  
+  # generate plot
   png(outputFileName, width=480, height=480)
   with(dataset, plot(datetime, Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)"))
   dev.off()
